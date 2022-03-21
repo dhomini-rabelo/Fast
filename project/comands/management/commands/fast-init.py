a9bd8007a3919e3867133827a572135666f1d295
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-21 10:37:45 -0300

    replace apps for __init__ incomands and update fast-init command

--- project/comands/management/commands/fast-init.py
+++ project/comands/management/commands/fast-init.py
@@ -14,12 +14,21 @@ class Command(BasicCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--del', '-d', action='store_true')
-        parser.add_argument('--worker', '-w', action='store_true')
 
     def handle(self, *args, **options):
+        actions = [
+            'creating folders for project',
+            'inserting important comments for Fast',
+            'changing  settings',
+            '   TEMPLATES["DIRS"], LANGUAGE_CODE, TIME_ZONE, STATICFILES_DIRS, STATIC_ROOT, MEDIA_ROOT',
+            '   MEDIA_URL, ACCOUNT_SESSION_REMEMBER',
+            'adapting archive urls.py',
+        ]
+        
         self.create_project_folders(settings.BASE_DIR, options)
         
         if options['del']:
+            actions.insert(1, 'deleting default coments')
             delete_comments_by_folder(str(settings.BASE_DIR), settings.PROJECT_NAME)
 
         project = DjangoProject(str(settings.BASE_DIR), settings.PROJECT_NAME)
@@ -28,15 +37,7 @@ class Command(BasicCommand):
         project.adapt_urls_py()
 
         display_tree(str(settings.BASE_DIR))
-        self.show_actions([
-            'creating folders for project',
-            'deleting default django comments',
-            'inserting important comments for Fast',
-            'changing  settings',
-            '   TEMPLATES["DIRS"], LANGUAGE_CODE, TIME_ZONE, STATICFILES_DIRS, STATIC_ROOT, MEDIA_ROOT',
-            '   MEDIA_URL, ACCOUNT_SESSION_REMEMBER',
-            'adapting archive urls.py',
-        ])
+        self.show_actions(actions)
 
     def create_project_folders(self, project_path: Path, options):
         folders = [
@@ -46,11 +47,13 @@ class Command(BasicCommand):
             'frontend/media',
             'frontend/static/styles',
             'frontend/static/styles/min',
-            'frontend/static/styles/apps',
+            'frontend/static/styles/bases',
+            'frontend/static/styles/pages',
             'frontend/static/styles/_compacts',
             'frontend/static/scripts',
             'frontend/static/scripts/min',
-            'frontend/static/scripts/apps',
+            'frontend/static/scripts/bases',
+            'frontend/static/scripts/pages',
             'frontend/static/scripts/_compacts',
             'frontend/templates',
             'frontend/templates/_compacts',
@@ -63,17 +66,6 @@ class Command(BasicCommand):
             'test/e2e',
             'test/dependencies',
         ]
-        if options['worker']:
-            folders += [
-                'worker',
-                'worker/api',
-                'worker/cache_pages',
-                'worker/static_pages_generator',
-                'test/worker',
-                'test/worker/api',
-                'test/worker/cache_pages',
-                'test/worker/static_pages_generator',
-            ]
 
         for folder in folders:
             new_path = Path(project_path, folder)
