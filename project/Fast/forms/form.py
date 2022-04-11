69c3e4078dbfbabce65d1ca24a97037691602a55
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-04-11 19:38:49 -0300

    BlockForm extends Form

--- project/Fast/forms/form.py
+++ project/Fast/forms/form.py
@@ -68,29 +68,14 @@ class Form:
 
 
 
-class BlockForm:
+class BlockForm(Form):
 
     def __init__(self):
-        self.form_fields: list[dict] = []
-        self.form = ''
+        super().__init__()
         self.blocks = {}
-        self.error_space_html = '<div class="error">'
-        self.error_message_format = '<span class="error-message"><img src="/static/admin/img/icon-no.svg" alt="error-img"></span>'
-        self.error_message_space_html = '<span class="error-message"><img src="/static/admin/img/icon-no.svg" alt="error-img">'
 
     def load_blocks(self, blocks: dict):
         self.blocks = blocks.copy()
-
-    def form_for_save(self):
-        return self.form_fields[:]
-    
-    def fast_load_form(self, form_fields: list[dict], processed_form: str):
-        self.form_fields = [item.copy() for item in form_fields]
-        self.form = processed_form[:]
-
-    def load_form(self, form_fields: list[dict]):
-        self.form_fields = [item.copy() for item in form_fields]
-        self._update_form()
     
     def load_form_with_values(self, form_fields: list[dict], values: dict):
         copy_form_fields = [item.copy() for item in form_fields]
@@ -131,8 +116,6 @@ class BlockForm:
         self.form_fields = copy_form_fields
         self._update_form()
         
-    def _update_form(self):
-        self.form = ''.join(map(lambda field: field["html"], self.form_fields))
 
     def add_fields(self, fields: list[dict]):
         for field in fields:
@@ -144,27 +127,6 @@ class BlockForm:
                 field_structure = field_structure.replace(place, field[key])
             self.form_fields.append({'name': field['name'], 'html': field_structure, 'block': block, 'history': [history]}) 
         self._update_form()
-
-    def show_errors(self, errors: dict):
-        for input in self.form_fields:
-            if input['name'] in errors.keys():
-                input['html'] = input['html'].replace(self.error_space_html, f'{self.error_space_html}{self.error_message_format}').replace(self.error_message_space_html, f'{self.error_message_space_html}{errors[input["name"]]}')
-        self._update_form()
-
-    def clear_form(self):
-        self.form = ''
-
-    def change_error_html(self, error_content: str, error_message_html: str, error_space: str):
-        """
-        self.error_space_html => div
-        self.error_message_space_html => span
-        """
-        self.error_space_html = error_content
-        self.error_message_format = error_message_html
-        self.error_message_space_html = error_space
-
-    def get_form(self):
-        return format_html(self.form)  
 
 
 
