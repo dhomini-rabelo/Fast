017e9e6b6189841bb6c7901938d5a3968d6ec50a
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-07 20:35:29 -0300

    ad base for tests

--- project/Fast/forms/adapt_data.py
+++ project/Fast/forms/adapt_data.py
@@ -1,7 +1,7 @@
 from typing import Callable
 from ..utils.main import get_decimal_str
 
-adapt_data_functions: dict[Callable] = {
+adapt_data_functions: dict[str, Callable] = {
     'int': lambda value: int(value),
     'str': lambda value: str(value),
     'decimal_2': lambda value: get_decimal_str(value),
